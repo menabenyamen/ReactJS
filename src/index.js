1dcc89d9b5ea029a00922bfb9bdaa0f0fd7332dc
MaWetter <malinwetter@gmail.com>
2017-10-09 14:53:07 +0200

    refactoring index-file (#7)
    updated index file in src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './reduxStore';
 import './common/css/master.css';
 import Home from './pages/Home';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Home />
+  </Provider>, document.getElementById('root'));
